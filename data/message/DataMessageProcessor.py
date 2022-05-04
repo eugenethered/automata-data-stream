af076a44d650a1f07790995e17fe369c59193b88
Eugene the Red <me@eugenethered.com>
2022-05-04 21:45:33 +0100

    fixing listen to stream

--- data/message/DataMessageProcessor.py
+++ data/message/DataMessageProcessor.py
@@ -4,7 +4,7 @@ class DataMessageProcessor:
         self.listen_to_stream = listen_to_stream
 
     def get_listen_to_stream(self):
-        pass
+        return self.listen_to_stream
 
     def process_message(self, message):
         pass
